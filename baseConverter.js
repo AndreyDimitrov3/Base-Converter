270b8e1bd4138ce122338a80e5aa48ce1f9a2cad
Andrey <158809049+AndreyDimitrov3@users.noreply.github.com>
2024-09-23 23:15:21 +0300

    Update baseConverter.js

--- baseConverter.js
+++ baseConverter.js
@@ -1,33 +1,37 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('search').addEventListener('click', performCalculations)
-    
-        function performCalculations() {
-            let input = document.getElementById('enterNumber').value;
-            let fromBase = document.getElementById('fromBase').value;
-            let toBase = document.getElementById('toBase').value;
 
-            if(fromBase < 17 && fromBase > 1 && toBase < 17 && toBase > 1) {
-                let decimalValue = convertFromBinaryToDecimal(input, fromBase);
-                let lowerBaseValue = convertFromDecimalToLower(decimalValue, toBase);
+    function performCalculations() {
+        let input = document.getElementById('enterNumber').value;
+        let fromBase = document.getElementById('fromBase').value;
+        let toBase = document.getElementById('toBase').value;
 
-                document.getElementById('answer').innerText = lowerBaseValue;
-            } else{
-                document.getElementById('answer').innerText = 'Enter correct values';
-            }
+        if(fromBase < 17 && fromBase > 1 && toBase < 17 && toBase > 1) {
+            let decimalValue = convertFromBinaryToDecimal(input, fromBase);
+            let lowerBaseValue = convertFromDecimalToLower(decimalValue, toBase);
+
+            document.getElementById('answer').innerText = lowerBaseValue;
+        } else{
+            document.getElementById('answer').innerText = 'Enter correct values';
         }
+    }
     
     function convertFromBinaryToDecimal(input, frBase) {
         let inputStr = String(input);
-        let nArr = Array.from(String(inputStr), Number);
-        let nArrDecimal = nArr.length - 1;
-    
+        let nArr = Array.from(String(inputStr), num => BigInt(num));
+        let nArrDecimal = BigInt(nArr.length - 1);
+        let base = BigInt(frBase);
+        
         for(let i = 0; i < nArr.length; i++) {
-            nArr[i] = nArr[i] * (Math.pow(frBase, nArrDecimal));
-            nArrDecimal -= 1;
+            nArr[i] = nArr[i] * (base ** nArrDecimal);
+            nArrDecimal -= 1n;
         }
     
-        return nArr.reduce((a, b) => a + b, 0);
+        let result = nArr.reduce((a, b) => a + b, 0n);
+        
+        return result;
     }
+    
     
     function convertFromDecimalToLower(input, radix) {
         let inputNumber = BigInt(input);
