f5ef4ec23609c9a636f570a97dc548af6a76de29
Andrey <158809049+AndreyDimitrov3@users.noreply.github.com>
2024-09-29 13:25:32 +0300

    Update onlyNumbers.js

--- scripts/onlyNumbers.js
+++ scripts/onlyNumbers.js
@@ -1,4 +1,4 @@
 function isNumberKey(evt) {
-    var charCode = (evt.which) ? evt.which : evt.keyCode
+    let charCode = (evt.which) ? evt.which : evt.keyCode;
     return !(charCode > 31 && (charCode < 48 || charCode > 57));
 }
